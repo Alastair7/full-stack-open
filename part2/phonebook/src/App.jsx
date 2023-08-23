26055ea98563f2af91752bf17eadc4e01f7a2e84
Alastair7 <alexandru.ban.drmer@gmail.com>
2023-08-23 19:31:14 +0200

    ADD: Part 2 Exercise 2.11 Phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,25 @@
 import Filter from "./Filter";
 import PersonForm from "./PersonForm";
 import Persons from "./Persons";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  const getPersons = () => {
+    console.log("Retrieving Persons from the server...");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("Promise Fullfilled");
+      setPersons(response.data);
+      console.log("Retrieved", response.data.length, "persons");
+    });
+  };
+
+  useEffect(getPersons, []);
 
   const personExists = (persons, name) => {
     console.log(
